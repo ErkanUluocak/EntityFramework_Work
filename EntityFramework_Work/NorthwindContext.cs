4b639a3077e4664eb0bc5a941d93d51ea5580190
Erkan Uluocak <erkan.uluocak@gmail.com>
2022-06-28 19:20:12 +0300

    Add project files.

---
+++ EntityFramework_Work/NorthwindContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_Work
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public class A
+    {
+        public virtual void Do()
+        {
+            //
+            //
+            //
+        }
+    }
+
+    public class B : A
+    {
+        public override void Do()
+        {
+            base.Do();
+        }
+    }
+}
